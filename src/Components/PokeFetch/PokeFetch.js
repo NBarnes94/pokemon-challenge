e40d28e61f0b244de3522beb20f068c055a7e47b
Nick Barnes <Nicholas.Barnes4194@.com>
2021-06-24 16:01:34 -0500

    working maybe not the way thats been asked but it works!

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import './PokeFetch.css';
 
 
-class PokeFetch extends Component {
+export default class PokeFetch extends Component {
   constructor() {
     super()
     this.state = {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      count: 15,
     }
   }
 
@@ -29,18 +30,58 @@ class PokeFetch extends Component {
       .catch((err) => console.log(err))
   }
 
+//   componentDidMount() {
+//       this.myInterval = setInterval(() =>
+//           this.setState({
+//             count: this.state.count -1
+//           }), 1000)
+// }
+
+  componentDidUpdate(preProps, prevState){
+    if (prevState.count == 1){
+      clearInterval(this.myInterval)
+    }
+  }
+
+  // componentWillUnmount(){
+  //   clearInterval(this.interval)
+  // }
+
+  resetCount = () =>{
+    this.setState({count: 15})
+    this.myInterval = setInterval(() =>
+          this.setState({
+            count: this.state.count -1
+          }), 1000)
+  }
+
+  handleSubmit(){
+    this.fetchPokemon();
+    this.resetCount();
+  }
   render() {
+    console.log(this.state);
+
     return (
       <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
+        <button className={'start'} onClick={() => this.handleSubmit() }>Start!</button>
+        <h1 className={'timer'} >{this.state.count > 0? 
+        <>
+        <div>
+        <h1>Time left!: {this.state.count}</h1>
+        <img className={'pokeImgDark'} src={this.state.pokeSprite} />
+        </div>
+        </>
+        :
+        <>
+        <h1> {this.state.pokeName} </h1>
+        <img className={'pokeImg'} src={this.state.pokeSprite} />
+        </>} </h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          {/* <img className={'pokeImg'} src={this.state.pokeSprite} /> */}
+          {/* <h1 className={'pokeName'}>{this.state.pokeName}</h1> */}
         </div>
       </div>
     )
   }
 }
-
-export default PokeFetch;
